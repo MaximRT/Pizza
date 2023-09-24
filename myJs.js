ec453514ccbffa47290cdb69167a5ea1aab4208a
Maxim Ponkratenkov <afkmaksim@gmail.com>
2023-09-24 15:10:21 +0300

    Pizza v2

--- myJs.js
+++ myJs.js
@@ -1,5 +1,19 @@
 const items = ['pizza 1', 'pizza 2', 'pizza 3'];
-items.push('pizza 4');
 items[1] = 'margarita';
-items.splice(0, 2);
+
+function add(pizza) {
+    items.push(pizza);
+    console.log(items);
+}
+
+function edit(position, newName) {
+    items[position] = newName;
+    console.log(items);
+}
+
+function remove(position) {
+    items.splice(position, 1);
+    console.log(items);
+}
+
 console.log(items);
